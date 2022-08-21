6858114735d0ad8083adb64cefea9a1c883e43cb
Jeffrey Asilo <jeffreyasilo.gss@gmail.com>
2022-08-21 22:24:21 +0800

    feat: Added react-query

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # React-Query-Practice
 
 Take note this sample project uses react-router v3 so and the latest version is already at v4 so take note of the new changes when starting from scratch.
+
+## V4 Changes
+
+- If we don't see network request wrap the key in the useQuery inside an array.
--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,27 @@
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+const fetchSuperHeroes = () => axios.get('http://localhost:4000/superheroes');
+
 export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>
-}
+  /* NOTES 
+    - The First Params is the key of the query
+    - Second params is the fetcher
+  */
+
+  const { isLoading, data } = useQuery('super-heroes', fetchSuperHeroes);
+
+  return (
+    <>
+      <h2>React Query Super Heroes Page</h2>
+      {/* NOTES data returns an object of our queried data */}
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : data ? (
+        data.data.map((hero) => <div key={hero.name}>{hero.name}</div>)
+      ) : (
+        <p>Heroes not found!</p>
+      )}
+    </>
+  );
+};
