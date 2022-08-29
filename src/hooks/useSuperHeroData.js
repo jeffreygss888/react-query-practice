d568a699329c678a89369ca9ff6779d43439003f
Jeffrey Asilo <jeffreyasilo.gss@gmail.com>
2022-08-29 16:37:59 +0800

    feat: Added Initial data on useQuery

--- src/hooks/useSuperHeroData.js
+++ src/hooks/useSuperHeroData.js
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 import axios from 'axios';
 
 // NOTES We can access queryKey from the useQuery since it automatically pass in the fetcher function
@@ -18,5 +18,25 @@ So thats why we pass in an array in our queryKey
 */
 
 export const useSuperHeroData = (heroId) => {
-  return useQuery(['super-hero', heroId], fetchSuperHero);
+  // NOTES This is how we put initial data on our query
+  // NOTES useQueryClient() is the instance of the QueryClient() we defined on the root app
+  // NOTES useful for SSR
+  const queryClient = useQueryClient();
+
+  return useQuery(['super-hero', heroId], fetchSuperHero, {
+    initialData: () => {
+      // NOTES We get the super-heroes queryKey data as our initial data
+      const heroData = queryClient
+        .getQueryData('super-heroes')
+        ?.data?.find((hero) => hero.id === parseInt(heroId));
+
+      // NOTES If there is a query of super-heroes queryKey data then we return than value
+      if (heroData) {
+        return { data: heroData };
+      } else {
+        // NOTES If there is no super-heroes queryKey then we return undefined
+        return undefined;
+      }
+    },
+  });
 };
